100f36386f5fb758dcb65d93c62526ff16b60965
Vaibhav Rajpoot <vaibhavrajpoot@Vaibhavs-MacBook-Air.local>
2024-11-02 09:15:44 +0530

    h

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -1,9 +1,12 @@
+import { assets } from '@/Assets/assets'
+import Image from 'next/image'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      FOOTER
+    <div className='flex justify-around flex-col gap-2 sm:gap-0 sm:flex-row bg-black py-5 items-center'>
+      <Image src={assets.logo_light} width={120} alt='' />
+      <p className='text-sm text-white'>Made and Designed by <span className='font-bold'>Vaibhav Rajpoot </span>💖</p>
     </div>
   )
 }
