1408b89a2bb78f493a718294f3c7f6afafc53624
Vaibhav Rajpoot <vaibhavrajpoot@Vaibhavs-MacBook-Air.local>
2024-11-03 18:14:55 +0530

    j

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -1,10 +1,12 @@
 "use client"
 
-import { blog_data } from '@/Assets/assets'
+import { assets, blog_data } from '@/Assets/assets'
+import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 
-const Page = ({ params }) => {
+const Page = ({ params: asyncParams }) => {
     const [data, setData] = useState(null)
+    const params = React.use(asyncParams)
 
     const fetchBlogData = () => {
         for (let i = 0; i < blog_data.length; i++) {
@@ -17,13 +19,24 @@ const Page = ({ params }) => {
     }
 
     useEffect(() => {
-        fetchBlogData()
+        if (params.id) {
+            fetchBlogData()
+        }
     }, [params.id])
-
-    return (
-        <div>
-            {data ? <div>{data.title}</div> : <div>Loading...</div>}
+    return ( data ? <>
+        <div className='bg-gray-200 md:px-12 lg:px-28 py-5 px-5'>
+            <div className='flex justify-between items-center'>
+                <Image src={assets.logo} alt='' width={180} className='w-[130px] sm:w-[180px]' />
+                <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#000000]'>
+                    Get Started
+                    <Image src={assets.arrow} alt='' />
+                    </button>
+            </div>
+            <div className='text-center py-24'>
+                <h1 className='text-2xl sm:text-5xl font-semibold max-w-[700px] mx-auto'>{data.title}</h1>
+            </div>
         </div>
+    </> : <></>
     )
 }
 
