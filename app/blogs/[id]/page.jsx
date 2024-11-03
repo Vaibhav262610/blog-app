70cd1f6a021f6461322eaff28dd213d85e11029e
Vaibhav Rajpoot <vaibhavrajpoot@Vaibhavs-MacBook-Air.local>
2024-11-03 18:30:16 +0530

    h

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -1,7 +1,9 @@
 "use client"
 
 import { assets, blog_data } from '@/Assets/assets'
+import Footer from '@/Components/Footer'
 import Image from 'next/image'
+import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 
 const Page = ({ params: asyncParams }) => {
@@ -26,8 +28,10 @@ const Page = ({ params: asyncParams }) => {
     return ( data ? <>
         <div className='bg-gray-200 md:px-12 lg:px-28 py-5 px-5'>
             <div className='flex justify-between items-center'>
+                <Link href='/'>
                 <Image src={assets.logo} alt='' width={180} className='w-[130px] sm:w-[180px]' />
-                <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#000000]'>
+                </Link>
+                <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black hover:shadow-[0px_0px_0px_#000000] shadow-[-7px_7px_0px_#000000] transition-all duration-175'>
                     Get Started
                     <Image src={assets.arrow} alt='' />
                     </button>
@@ -35,9 +39,15 @@ const Page = ({ params: asyncParams }) => {
             <div className='text-center py-24'>
                 <h1 className='text-2xl sm:text-5xl font-semibold max-w-[700px] mx-auto'>{data.title}</h1>
                 <Image className='mx-auto  mt-6 border rounded-full border-white' src={data.author_img} alt='' width={60} height={60}/>
-                <p>{data.author}</p>
+                <p className='mt-1 pb-2 text-lg max-w-[740px] mx-auto'>{data.author}</p>
             </div>
         </div>
+            <div className='mx-5 max-w-[800px] md:mx-auto mt-[-100px] mb-10'>
+                <Image className='border-4 border-white' src={data.image} alt='' width={1280} height={720} />
+                <h1 className='my-8 text-[26px] font-semibold'>Introduction : </h1>
+                <p>{data.description}</p>
+            </div>
+            <Footer />
     </> : <></>
     )
 }
