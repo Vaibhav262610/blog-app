731c7b268d14d582c369e714be8366a7248aadb5
Vaibhav Rajpoot <vaibhavrajpoot@Vaibhavs-MacBook-Air.local>
2024-11-09 13:40:19 +0530

    h

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -1,4 +1,5 @@
 import { ConnectDB } from "@/lib/config/db";
+import BlogModel from "@/lib/models/BlogModel";
 
 const { NextResponse } = require("next/server");
 
@@ -12,10 +13,35 @@ LoadDB()
 export async function GET(request) {
     console.log("BLOG GET HIT");
     return NextResponse.json({msg:"API WORKING "})
-    
+
 }
 
 export async function POST(request) {
-    const formData = await request.formData()
-    const timestamp = Date.now()
-}
+    const formData = await request.formData();
+    const timestamp = Date.now();
+
+
+    const image = formData.get('Image');
+    const imageByData = await image.arrayBuffer();
+    const buffer = Buffer.from(imageByData);
+    const path = `./public/${timestamp}_${image.name}`;
+    await writeFile(path,buffer);
+    const imgURL = `/${timestamp}_${image.name}`;
+
+    const blogData = {
+        title :`${formData.get('title')}`,
+        description:`${formData.get('description')}`,
+        category:`${formData.get('category')}`,
+        author:`${formData.get('author')}`,
+        image:`${imgURL}`,
+        authorImg:`${formData.get('authorImg')}`,
+    }
+
+
+    await BlogModel.create(blogData)
+    console.log("Blog Saved");
+
+
+    return NextResponse.json({success:true , msg :"Blog Added"})
+};
+
--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
+
+
 
 const page = () => {
+
+  const [email, setEmail] = useState('')
+
+  const getEmail = () => {
+    console.log(email); 
+  }
+
+  const handleInputChange = (event) => {
+    setEmail(event.target.value);
+};
+
   return (
     <div className='flex justify-center items-center w-full h-screen'>
       <div className='flex flex-col justify-center items-center'>
-       <h1 className='text-5xl font-semibold'>Create an Account</h1>
+       <h1 className='text-3xl font-semibold'>Create an Account</h1>
        <h2 className='text-sm mt-2 text-gray-400'>Enter your email below to create your account</h2>
        <form action="" typeof='submit' className='flex flex-col'>
-        <input type="email" placeholder='name@example.com' className='border w-[320px] font-light py-1  px-3 outline-none  text-gray-600 border-gray-700 rounded-lg mt-7' />
-        <button className='border border-black shadow-[-7px_7px_0px_#000000] hover:shadow-[0px_0px_0px_#000000] duration-100 px-5 py-1 mt-4 '>Sign In with Email</button>
+        <input value={email} onChange={handleInputChange} type="email" placeholder='name@example.com' className='border w-[320px] font-light py-1  px-3 outline-none  text-gray-600 border-gray-700 mt-5' />
+        <button onClick={getEmail} className='border border-black shadow-[-7px_7px_0px_#000000] hover:shadow-[0px_0px_0px_#000000] duration-100 px-5 py-1 mt-4 '>Sign In with Email</button>
        </form>
        <h2 className='text-xs mt-4 uppercase'>or continue with</h2>
       </div>
