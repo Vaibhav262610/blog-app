7e8dddb5b5e15c2671ec3a7400b63a4f4130c58b
Vaibhav Rajpoot <vaibhavrajpoot@Vaibhavs-MacBook-Air.local>
2024-11-11 12:50:03 +0530

    h

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -1,5 +1,5 @@
 import { ConnectDB } from "@/lib/config/db";
-
+import {writeFile} from 'fs/promises'
 const { NextResponse } = require("next/server");
 
 
@@ -18,4 +18,14 @@ export async function GET(request) {
 export async function POST(request) {
     const formData = await request.formData()
     const timestamp = Date.now()
+
+    const image = formData.get('image')
+    const imageByteData = await image.arrayBuffer()
+    const buffer = Buffer.from(imageByteData)
+    const path = `./public/${timestamp}_${image.name}`
+    await writeFile(path,buffer)
+    const imgUrl = `/${timestamp}_${image.name}`
+    console.log(imgUrl);
+    return NextResponse.json({imgUrl})
+    
 }
--- app/login/page.jsx
+++ app/login/page.jsx
@@ -4,14 +4,19 @@ const page = () => {
   return (
     <div className='flex justify-center items-center w-full h-screen'>
       <div className='flex flex-col justify-center items-center'>
-       <h1 className='text-5xl font-semibold'>Create an Account</h1>
+       <h1 className='text-2xl font-semibold'>Create an Account</h1>
        <h2 className='text-sm mt-2 text-gray-400'>Enter your email below to create your account</h2>
        <form action="" typeof='submit' className='flex flex-col'>
-        <input type="email" placeholder='name@example.com' className='border w-[320px] font-light py-1  px-3 outline-none  text-gray-600 border-gray-700 rounded-lg mt-7' />
+        <input type="email" placeholder='name@example.com' className='border w-[320px] font-light py-1  px-3 outline-none  text-gray-600 border-gray-700 mt-7' />
+        <button className='border border-black shadow-[-7px_7px_0px_#000000] hover:shadow-[0px_0px_0px_#000000] duration-100 px-5 py-1 mt-4 '>Sign In with Email</button>
+       <div className='flex justify-center items-center'>
+        <hr className='w-24 text-black mt-4 fill-black' />
+       <h2 className='text-xs mt-4 uppercase'>or continue with</h2>
+        <hr className='w-24 text-black mt-4 fill-black' />
+       </div>
         <button className='border border-black shadow-[-7px_7px_0px_#000000] hover:shadow-[0px_0px_0px_#000000] duration-100 px-5 py-1 mt-4 '>Sign In with Email</button>
        </form>
-       <h2 className='text-xs mt-4 uppercase'>or continue with</h2>
-      </div>
+       </div>
     </div>
   )
 }
